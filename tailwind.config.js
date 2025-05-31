0b204fa88df344b80a6e28f644a74bc51654b458
Marcos Campos <marcamsn@gmail.com>
2025-05-31 12:30:47 +0200

    Add supabaseClient, tailwind.css and update dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/@hero-ui/react/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
